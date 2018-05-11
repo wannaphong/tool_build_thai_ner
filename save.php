78be836b1adb24f6ab9ac0f8fc299362ae641512
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2018-05-12 01:55:15 +0700

    save file

--- save.php
+++ save.php
@@ -1,6 +1,6 @@
 <?php
 include("src/Medoo.php");
-$text = $_POST['text'];
+$text = $_POST['edit'];
 $username= $_POST['username'];
 $email = $_POST['email'];
 use Medoo\Medoo;
@@ -11,11 +11,12 @@ $database = new Medoo([
 foreach(preg_split("/((\r?\n)|(\r\n?))/", $text) as $line){
     // do stuff with $line
     $database->insert("data", [ # ตาราง usertbl
-    "text" => $line,
-    "username" => $username,
-    "email" => $email,
+    "text" => htmlspecialchars($line),
+    "username" => htmlspecialchars($username),
+    "email" => htmlspecialchars($email),
     "ip_address" => $_SERVER['REMOTE_ADDR'],
     "check" => 0,
    ]);
 }
+header("index.html");
 ?>
