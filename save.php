24a6e8c245edb971b111aabc28098ac55d3a383a
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2018-05-11 22:19:34 +0700

    add save data

---
+++ save.php
@@ -0,0 +1,21 @@
+<?php
+include("src/Medoo.php");
+$text = $_POST['text'];
+$username= $_POST['username'];
+$email = $_POST['email'];
+use Medoo\Medoo;
+$database = new Medoo([
+	'database_type' => 'sqlite',
+	'database_file' => 'db.sqlite3'
+]);
+foreach(preg_split("/((\r?\n)|(\r\n?))/", $text) as $line){
+    // do stuff with $line
+    $database->insert("data", [ # ตาราง usertbl
+    "text" => $line,
+    "username" => $username,
+    "email" => $email,
+    "ip_address" => $_SERVER['REMOTE_ADDR'],
+    "check" => 0,
+   ]);
+}
+?>
